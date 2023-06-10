5f69d033cd6b5920fe9f09b9a74774d871a0bc19
Jarer Zeenam <jareerzeenam.28@gmail.com>
2023-06-11 00:01:17 +0530

    initializing layouts done

---
+++ app/posts/layout.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const layout = () => {
+  return <div>Navigate To Top</div>;
+};
+
+export default layout;
---
+++ app/posts/new/page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>NEW POST</div>;
+};
+
+export default page;
