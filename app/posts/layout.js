b057d2a7a4eaf718367594ca0974af7aebb24dd6
Jarer Zeenam <jareerzeenam.28@gmail.com>
2023-06-11 19:23:35 +0530

    Initial folder structure and packages added

--- app/posts/layout.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const layout = () => {
-  return <div>Navigate To Top</div>;
-};
-
-export default layout;
--- app/posts/new/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const page = () => {
-  return <div>NEW POST</div>;
-};
-
-export default page;
