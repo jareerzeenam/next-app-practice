e6dba0efa21c83115138da91d91cb7b1d467bfa0
Jarer Zeenam <jareerzeenam.28@gmail.com>
2023-06-21 16:41:53 +0530

    Next js API Authentication implemented  using Google cloud and save to MongoDB

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Progress (01:15:45) [Video](https://youtu.be/wm5gMKuwSYk)
+# Progress (01:49:20) [Video](https://youtu.be/wm5gMKuwSYk)
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,19 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
   
-  const isUserLoggedIn = true;
+  const { data:session } = useSession();
+
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
   useEffect(()=>{
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
 
       setProviders(response)
     }
 
-    setProviders()
+    setUpProviders()
   },[])
 
   return (
@@ -35,7 +36,7 @@ const Nav = () => {
 
       { /* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        { session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post 
@@ -47,7 +48,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -73,10 +74,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
              <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
