e86c545133ccf25d5dc391106ffcf93aab607835
Jarer Zeenam <jareerzeenam.28@gmail.com>
2023-07-03 22:55:20 +0530

    Prompt CRUD completed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,13 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
+  // Get current session 
+  const { data: session } = useSession();
+  // Get current path name
+  const pathName = usePathname();
+
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -48,8 +55,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
       <p className='font-inter text-sm blue_gradient cursor-pointer'
       onClick={() => handleTagClick && handleTagClick(post.tag)}>
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p 
+          className='font-inter text-sm green_gradient cursor-pointer'
+          onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+          className='font-inter text-sm orange_gradient cursor-pointer'
+          onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
