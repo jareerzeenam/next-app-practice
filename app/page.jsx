f8053df63c39c9c515f92f5c7c32029aec1f639d
Jareer Zeenam <jareerzeenam.28@gmail.com>
2023-12-20 18:45:55 +0000

    COntent updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,22 @@
-import Feed from '@components/Feed'
+import Feed from '@components/Feed';
 
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
-            Discover & Share
-            <br className="max-md:hidden" />
-            <span className="orange_gradient text-center">AI-Powered Prompts</span>
-        </h1>
-        <p className="desc text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus earum incidunt voluptate?
-        </p>
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">
+          AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        <i>Excellent Prompt Yields Excellent Response.</i>
+      </p>
 
-        <Feed/>
+      <Feed />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
